bb1ab79707f085f4baf4090c2235746969d64ed7
DYung26 <theadstore1407@gmail.com>
2024-05-03 06:31:47 -0700

    a Bash script that sets up your web servers for the deployment of `web_static`.

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+sudo apt-get install nginx -y
+# sudo mkdir -p /data/
+# sudo mkdir -p /data/web_static/
+# sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Hello DYung!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+if [ ! -L "/data/web_static/current" ]; then
+    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/^\t*server_name _;/a\\t\/location \/hbnb_static {\n\t\talias \/data\/web_static\/current\/hbnb_static\/;\n\t\tindex index.html;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
