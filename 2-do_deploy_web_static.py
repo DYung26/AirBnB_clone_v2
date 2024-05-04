3c4b98290d2eb2f10dad18dec02478bf669cc1ac
DYung26 <theadstore1407@gmail.com>
2024-05-03 21:11:02 -0700

    a Fabric script (based on the file `1-pack_web_static.py`) that distributes an archive to my web servers, using the function `do_deploy`.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,11 +4,12 @@ Using Fabric to distribut an archive to my web servers,
 using the function `do_deploy`
 """
 from fabric.state import env
-from fabric.api import local, put, connect
+from fabric.api import local, put, run
 import os
 
 env.hosts = ['34.207.64.86', '54.158.217.146']
 env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa.pub'
 
 
 def do_deploy(archive_path):
@@ -17,25 +18,24 @@ def do_deploy(archive_path):
     """
     try:
         for host in env.hosts:
-            c = connect(host)
             if not os.path.exists(archive_path):
                 return False
             filename = archive_path.replace("versions/",
                                             "").replace(".tgz", "").strip()
             base_path = '/data/web_static/'
             put(archive_path, '/tmp/')
-            c.run('mkdir  -p {0}releases/{1}'.format(base_path, filename))
-            c.run('tar -xzf {1} -C {0}releases/{2}'.format(base_path,
+            run('mkdir  -p {0}releases/{1}'.format(base_path, filename))
+            run('tar -xzf {1} -C {0}releases/{2}'.format(base_path,
                                                            archive_path,
                                                            filename))
-            c.run('rm /tmp/{}'.format(archive_path))
-            c.run('mv {0}releases/{1}/web_static/* '
+            run('rm /tmp/{}'.format(archive_path))
+            run('mv {0}releases/{1}/web_static/* '
                   '{0}releases/{1}/'.format(base_path,
                                             filename))
-            c.run('rm -rf {0}releases/{1}/web_static/'.format(base_path,
+            run('rm -rf {0}releases/{1}/web_static/'.format(base_path,
                                                               filename))
-            c.run('rm -rf /data/web_static/current')
-            c.run('ln -s {0}releases/{1} {0}current'.format(base_path,
+            run('rm -rf /data/web_static/current')
+            run('ln -s {0}releases/{1} {0}current'.format(base_path,
                                                             filename))
             print("New version deployed")
         return True
