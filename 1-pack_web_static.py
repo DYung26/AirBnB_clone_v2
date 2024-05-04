30e53c16e57e7b72967c2a0a190665e2b32d9662
DYung26 <theadstore1407@gmail.com>
2024-05-03 18:19:57 -0700

    a Fabric script that generates a `.tgz` archive from the contents of the `web_static` folder.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,11 @@ def do_pack():
     date = local('date +"%Y%m%d%H%M%S"', capture=True)
     folder = "web_static"
     archive = "versions/web_static_{}.tgz".format(date)
+    #res = local('ls -R web_static', capture=True)
+    #lines = res.splitlines()
+
+    #for line in lines:
+    #    print(line)
     result = local("tar -cvzf {} {}".format(archive, folder), capture=True)
 
     if result.succeeded:
