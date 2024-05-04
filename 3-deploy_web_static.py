9a99072188ef8c03350bf55fb6a4c3e4692d085c
DYung26 <theadstore1407@gmail.com>
2024-05-03 21:55:54 -0700

    a Fabric script (based on the file `2-do_deploy_web_static.py`) that creates and distributes an archive to your web servers, using the function `deploy`.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,17 +3,21 @@
 Creates and distributes an archive to your web servers,
 using the function `deploy`.
 """
-from fabric import env
+from fabric.state import env
 from fabric.api import local, run
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+import importlib
+module_name = "1-pack_web_static"
+module = importlib.import_module(module_name)
+module_name1 = "2-do_deploy_web_static"
+module1 = importlib.import_module(module_name1)
+
 
 def deploy():
     """
         Creates and distributes an archive to my web servers.
     """
-    path = do_pack()
+    path = module.do_pack()
     if path is False:
         return False
-    ret_val = do_deploy(path)
+    ret_val = module1.do_deploy(path)
     return ret_val
